fa945a8a500d1209b05dc177277265dcf335b975
Eric Ting <erictingting2000@gmail.com>
2024-04-24 15:52:55 +0800

    feat: mod gh pages deploy setting

--- next.config.js
+++ next.config.js
@@ -2,18 +2,21 @@ const { resolve } = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // experimental: {
-    //     esmExternals: 'loose',
-    // },
-    // webpack: (config, { isServer }) => {
-    //     console.log('@web3-onboard/react', require.resolve('@web3-onboard/react'))
-    //     config.resolve.alias = {
-    //         ...config.resolve.alias,
-    //         '@web3-onboard/react':require.resolve('@web3-onboard/react'),
-    //     }
+  output: 'export',
+  reactStrictMode: true,
+  distDir: 'dist',
+  // experimental: {
+  //     esmExternals: 'loose',
+  // },
+  // webpack: (config, { isServer }) => {
+  //     console.log('@web3-onboard/react', require.resolve('@web3-onboard/react'))
+  //     config.resolve.alias = {
+  //         ...config.resolve.alias,
+  //         '@web3-onboard/react':require.resolve('@web3-onboard/react'),
+  //     }
 
-    //     return config;
-    // }
-}
+  //     return config;
+  // }
+};
 
 module.exports = nextConfig
